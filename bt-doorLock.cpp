ddc2e74e8d088c9ceb4fd11309ee720d2a91b30f
Chiebuka Lebechi <chiebuka.lebechi@gmail.com>
2018-11-11 10:44:21 -0900

    original file

---
+++ bt-doorLock.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+
+int main(){
+
+    cout << "This program locks/unlocks a door if a nearby trusted bluetooth devices is detected." << endl;
+
+    
+
+    return 0;
+}
